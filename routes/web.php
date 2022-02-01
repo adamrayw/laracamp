ca6953f5cbe32f6e08dbe92b5b1eb9b68c4aedd2
Adam Ray <raywibowo68@gmail.com>
2022-02-01 22:07:31 +0700

    Add middleware group only for logged user for checkout and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,21 +19,22 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout/{camp:slug}', function () {
-    return view('checkout');
-})->name('checkout');
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
 
 // socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
-
 Route::get('auth/google/callback', [UserController::class, 'handleUserCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
+
+Route::middleware(['auth'])->group(function () {
+    // chekout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    // user routes
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 require __DIR__ . '/auth.php';
