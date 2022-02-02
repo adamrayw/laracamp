c8a4dfac282165ec9803ed1f42528334b02f5756
Adam Ray <raywibowo68@gmail.com>
2022-02-02 19:11:49 +0700

    Using whereEmail method for check if the email already registered, if not,  create the user and send an email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\User\AfterRegister;
 
 class UserController extends Controller
 {
@@ -29,7 +31,17 @@ class UserController extends Controller
             'email_verified_at' => date('Y-m-d H:i:s', time())
         ];
 
-        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        // $user = User::firstOrCreate(['email' => $data['email']], $data);
+
+        // cek apakah email tersebut sudah ada di database
+        $user = User::whereEmail([$data['email']])->first();
+
+        // jika email tidak ada di database, maka tambahkan email tersebut ke database
+        // dan kirim email ke email tersebut
+        if (!$user) {
+            $user = User::create($data);
+            Mail::to($user->email)->send(new AfterRegister($user));
+        }
         Auth::login($user, true);
 
         return redirect(route('welcome'));
