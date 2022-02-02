2cb07c0abbb3a1e8acf32cfe94df7c77f204c944
Adam Ray <raywibowo68@gmail.com>
2022-02-02 19:53:12 +0700

    Add new route for user can check invoice

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::middleware(['auth'])->group(function () {
 
     // user routes
     Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard/checkout/invoice/{checkout}', [CheckoutController::class, 'invoice'])->name('user.checkout.invoice');
 });
 
 require __DIR__ . '/auth.php';
