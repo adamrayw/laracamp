2759dfa4c6c8e6c6601e1fa1e8729a87e37d9ab9
Adam Ray <raywibowo68@gmail.com>
2022-02-02 22:15:56 +0700

    Admin DashboardController

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Checkout;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $checkout = Checkout::with('Camp')->get();
+
+        return view('admin.dashboard', [
+            'checkouts' => $checkout,
+        ]);
+    }
+}
